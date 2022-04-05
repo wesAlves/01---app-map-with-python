24739006a2399549f98e177af9987fec125051e9
Wes <wal2.designer@gmail.com>
2022-04-05 20:15:45 +0100

    added population and poligons to the map

--- map1.py
+++ map1.py
@@ -27,15 +27,12 @@ def color_by_elevation(elev):
 map = folium.Map(location=[38.58, -99.09],
                  zoom_start=6, tiles='Stamen Terrain')
 
-feature_group = folium.FeatureGroup(name='My Map')
+vulcanos = folium.FeatureGroup(name='Vulcanos')
+population = folium.FeatureGroup(name='Population')
 
 for lat, long, elev in zip(latitude, longetude, elevation):
 
-    # feature_group.add_child(folium.Marker(
-    #     location=[lat, long],
-    #     popup=f'Elevation: {elev} m',
-    #     icon=folium.Icon(color=color_by_elevation(elev))))
-    feature_group.add_child(
+    vulcanos.add_child(
         folium.Circle(
             location=[lat, long],
             radius=elev,
@@ -45,6 +42,21 @@ for lat, long, elev in zip(latitude, longetude, elevation):
         )
     )
 
-map.add_child(feature_group)
+population.add_child(folium.GeoJson(
+    data=open('world.json', 'r', encoding='utf-8-sig').read(),
+    # lambda is an anonymous function that create a possible to createand call a functuin in one line
+    style_function=lambda x: {
+        'fillColor': 'green' if x['properties']['POP2005'] < 1000000
+        else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000
+        else 'red'
+    }
+))
+
+
+map.add_child(vulcanos)
+map.add_child(population)
+
+# this layer control needs to come after the feature_group, otherwise it will not find the layers
+map.add_child(folium.LayerControl())
 
 map.save("Map.html")
